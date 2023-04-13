4334d24669e460e44984b8ff66038c8953140d96
vumg <ab17100222@gmail.com>
2023-04-12 18:20:36 -0600

    animacion test

--- scripts/index.js
+++ scripts/index.js
@@ -14,7 +14,7 @@ enter.addEventListener('click',enterSite, false);
 
 function enterSite(){
  
-  tls.to('#introImage', {y:"100%",duration:.6, stagger:0.25});
   tls.to('.intro',{y:"100%",duration:.6,stagger:0.25}); 
+  tls.to('#introImage', {y:"150%",duration:.6, stagger:0.25});
   tls.to('#textPage',{y:"0%",duration:.6,stagger:0.25}); 
 }
